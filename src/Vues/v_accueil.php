87b4fe474a7a67e3b8a7c98b1f1df5816d42af87
Maxime Vernoux <mxm.vernoux@gmail.com>
2023-10-19 12:04:24 +0200

    button adapté comptable

--- src/Vues/v_accueil.php
+++ src/Vues/v_accueil.php
@@ -38,14 +38,25 @@
             <div class="panel-body">
                 <div class="row">
                     <div class="col-xs-12 col-md-12">
-                        <a href="index.php?uc=gererFrais&action=saisirFrais"
-                           class="btn btn-success btn-lg" role="button">
-                            <span class="glyphicon glyphicon-pencil"></span>
-                            <br>Renseigner la fiche de frais</a>
-                        <a href="index.php?uc=etatFrais&action=selectionnerMois"
-                           class="btn btn-primary btn-lg" role="button">
-                            <span class="glyphicon glyphicon-list-alt"></span>
-                            <br>Afficher mes fiches de frais</a>
+                        <?php if ($_SESSION['metier'] == 'visiteur') {?>
+                            <a href="index.php?uc=gererFrais&action=saisirFrais"
+                               class="btn btn-success btn-lg" role="button">
+                                <span class="glyphicon glyphicon-pencil"></span>
+                                <br>Renseigner la fiche de frais</a>
+                            <a href="index.php?uc=etatFrais&action=selectionnerMois"
+                               class="btn btn-primary btn-lg" role="button">
+                                <span class="glyphicon glyphicon-list-alt"></span>
+                                <br>Afficher mes fiches de frais</a>
+                        <?php } elseif ($_SESSION['metier'] == 'comptable') {?>
+                            <a href="index.php?uc=validerFrais&action=selectionnerMoisVisiteur"
+                               class="btn btn-success btn-lg" role="button">
+                                <span class="glyphicon glyphicon-ok"></span>
+                                <br>Valider les fiches de frais</a>
+                            <a href="index.php?uc=suiviFicheFrais&action=actionADefinir"
+                               class="btn btn-primary btn-lg" role="button">
+                                <span class="glyphicon glyphicon-euro"></span>
+                                <br>Suivre le paiement des fiches de frais</a>
+                        <?php } ?>
                     </div>
                 </div>
             </div>
