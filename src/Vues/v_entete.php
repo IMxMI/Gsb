1f899d86edf752bb140b34a9b6919052aecdff77
Maxime Vernoux <mxm.vernoux@gmail.com>
2023-11-16 09:40:30 +0100

    css

--- src/Vues/v_entete.php
+++ src/Vues/v_entete.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Vue Entête
  *
@@ -15,7 +14,6 @@
  * @link      http://www.reseaucerta.org Contexte « Laboratoire GSB »
  * @link      https://getbootstrap.com/docs/3.3/ Documentation Bootstrap v3
  */
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -34,65 +32,70 @@
             <?php
             $uc = filter_input(INPUT_GET, 'uc', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
             if ($estConnecte) {
+                $active = 'active';
+                if ($_SESSION['metier'] == 'comptable') {
+                    $active = 'activecomptable';
+                }
                 ?>
-            <div class="header">
-                <div class="row vertical-align">
-                    <div class="col-md-4">
-                        <h1>
-                            <img src="./images/logo.jpg" class="img-responsive" 
-                                 alt="Laboratoire Galaxy-Swiss Bourdin" 
-                                 title="Laboratoire Galaxy-Swiss Bourdin">
-                        </h1>
-                    </div>
-                    <div class="col-md-8">
-                        <ul class="nav nav-pills pull-right" role="tablist">
-                            <li <?php if (!$uc || $uc == 'accueil') { ?>class="active" <?php } ?>>
-                                <a href="index.php">
-                                    <span class="glyphicon glyphicon-home"></span>
-                                    Accueil
-                                </a>
-                            </li>
-                            <?php if ($_SESSION['metier'] == 'visiteur') { ?>
-                                <li <?php if ($uc == 'gererFrais') { ?>class="active"<?php } ?>>
-                                    <a href="index.php?uc=gererFrais&action=saisirFrais">
-                                        <span class="glyphicon glyphicon-pencil"></span>
-                                        Renseigner la fiche de frais
+                <div class="header">
+                    <div class="row vertical-align">
+                        <div class="col-md-4">
+                            <h1>
+                                <img src="./images/logo.jpg" class="img-responsive" 
+                                     alt="Laboratoire Galaxy-Swiss Bourdin" 
+                                     title="Laboratoire Galaxy-Swiss Bourdin">
+                            </h1>
+                        </div>
+                        <div class="col-md-8">
+                            <ul class="nav nav-pills pull-right <?php if ($_SESSION['metier'] == 'comptable') { ?>comptable<?php } ?>" role="tablist">
+                                <li class="<?php if ($uc == 'accueil' || !$uc) {echo $active;}?>">
+                                    <a href="index.php">
+                                        <span class="glyphicon glyphicon-home"></span>
+                                        Accueil
                                     </a>
                                 </li>
-                                <li <?php if ($uc == 'etatFrais') { ?>class="active"<?php } ?>>
-                                    <a href="index.php?uc=etatFrais&action=selectionnerMois">
-                                        <span class="glyphicon glyphicon-list-alt"></span>
-                                        Afficher mes fiches de frais
+                                <?php if ($_SESSION['metier'] == 'visiteur') { ?>
+                                    <li class="<?php if ($uc == 'gererFrais') {echo $active;}?>">
+                                        <a href="index.php?uc=gererFrais&action=saisirFrais">
+                                            <span class="glyphicon glyphicon-pencil"></span>
+                                            Renseigner la fiche de frais
+                                        </a>
+                                    </li>
+                                    <li class="<?php if ($uc == 'etatFrais') {echo $active;}?>">
+                                        <a href="index.php?uc=etatFrais&action=selectionnerMois">
+                                            <span class="glyphicon glyphicon-list-alt"></span>
+                                            Afficher mes fiches de frais
+                                        </a>
+                                    </li>
+                                <?php } elseif ($_SESSION['metier'] == 'comptable') { ?>
+                                    <li class="<?php if ($uc == 'validerFrais') {echo $active;}?>">
+                                        <a href="index.php?uc=validerFrais&action=selectionnerMoisVisiteur"
+                                            <span class="glyphicon glyphicon-ok"></span>
+                                            Valider les fiches de frais
+                                        </a>
+                                    </li>
+                                    <li class="<?php if ($uc == 'suiviFicheFrais') {echo $active;}?>">
+                                        <a href="index.php?uc=suiviFicheFrais&action=actionADefinir"
+                                            <span class="glyphicon glyphicon-euro"></span>
+                                            Suivre le paiement des fiches de frais
+                                        </a>
+                                    </li>
+                                <?php } ?>
+                                <li class="<?php if ($uc == 'deconnexion') {echo $active;}?>">
+                                    <a href="index.php?uc=deconnexion&action=demandeDeconnexion" <?php
+                                    if ($uc == 'deconnexion') {
+                                        $active;
+                                    }
+                                    ?>>
+                                        <span class="glyphicon glyphicon-log-out"></span>
+                                        Déconnexion
                                     </a>
                                 </li>
-                            <?php } elseif ($_SESSION['metier'] == 'comptable') { ?>
-                                <li <?php if ($uc == 'validerFrais') { ?>class="active"<?php } ?>>
-                                    <a href="index.php?uc=validerFrais&action=selectionnerMoisVisiteur" class="comptable">
-                                        <span class="glyphicon glyphicon-ok"></span>
-                                        Valider les fiches de frais
-                                    </a>
-                                </li>
-                                <li <?php if ($uc == 'suiviFicheFrais') { ?>class="active"<?php } ?>>
-                                    <a href="index.php?uc=suiviFicheFrais&action=actionADefinir">
-                                        <span class="glyphicon glyphicon-euro"></span>
-                                        Suivre le paiement des fiches de frais
-                                    </a>
-                                </li>
-                            <?php } ?>
-                            <li 
-                            <?php if ($uc == 'deconnexion') { ?>class="active"<?php } ?>>
-                                <a href="index.php?uc=deconnexion&action=demandeDeconnexion">
-                                    <span class="glyphicon glyphicon-log-out"></span>
-                                    Déconnexion
-                                </a>
-                            </li>
-                        </ul>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <?php
-            } else {
-                ?>   
+                <?php } else { ?>   
                 <h1>
                     <img src="./images/logo.jpg"
                          class="img-responsive center-block"
