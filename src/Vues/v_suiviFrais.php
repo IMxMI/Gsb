09a66f9cee3540019208514c2df6d44f4b47c84f
Maxime Vernoux <mxm.vernoux@gmail.com>
2023-11-18 19:04:40 +0100

    Tableau suiviFrais

--- src/Vues/v_suiviFrais.php
+++ src/Vues/v_suiviFrais.php
@@ -1,4 +1,33 @@
-<?php
+<?php ?>
+<table>
+    <thead>
+        <tr>
+            <td>Visiteurs</td>
+            <td>Mois</td>
+            <td>Total forfait</td>
+            <td>Total hors forfait</td>
+            <td>Montant validé</td>
+            <td>Mise en paiement</td>
+        </tr>
+    </thead>
+    <tbody>
 
-var_dump($testFiche);
+        <?php
+        foreach ($testFiche as $test) {
+            ?>
+            <tr>
+                <td><?php
+                    echo $test['nom'];
+                    echo $test['prenom'];
+                    ?></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        <?php };
+        ?>
+    </tbody>
+</table>
 
