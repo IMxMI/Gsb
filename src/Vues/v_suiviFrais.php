1763e81e6d04b7cfde01a6cf4125663f7b2bb313
Maxime Vernoux <mxm.vernoux@gmail.com>
2023-11-18 20:35:50 +0100

    checkbox suivifrais

--- src/Vues/v_suiviFrais.php
+++ src/Vues/v_suiviFrais.php
@@ -1,4 +1,5 @@
 <?php ?>
+<button class="label label-default"></button>
 <div class="panel panel-warning">
     <div class="panel-heading">Liste des fiches de frais validées</div>
     <table class="table table-striped table-hover">
@@ -9,14 +10,14 @@
                 <th>Total forfait</th>
                 <th>Total hors forfait</th>
                 <th>Montant validé</th>
-                <th>Mise en paiement</th>
+                <th><center>Mise en paiement</center></th>
             </tr>
         </thead>
         <tbody>
             <?php
             foreach ($testFiche as $test) {
                 ?>
-                <tr id="<?php echo $test['idvisiteur'] . '-' . $test['mois'] ?>">
+                <tr idLigne="<?php echo $test['idvisiteur'] . '-' . $test['mois'] ?>">
                     <td><?php
                         echo $test['nom'] . ' ';
                         echo $test['prenom'];
@@ -29,7 +30,7 @@
                     <td><?php
                         echo $test['montantvalide'] . '€';
                         ?></td>
-                    <td></td>
+                    <td><center><input type="checkbox" idCheckBox="<?php echo $test['idvisiteur'] . '-' . $test['mois'] ?>"></center></td>
                 </tr>
             <?php };
             ?>
