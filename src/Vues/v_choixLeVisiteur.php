fc65840072532cb407d412f51083003d3692b1df
kaoutharlehhit <119665328+kaoutharlehhit@users.noreply.github.com>
2023-12-21 00:05:30 +0100

    rajout de l'option choisir sur la liste déroulate mais il y a un soucis au niveau du mois

--- src/Vues/v_choixLeVisiteur.php
+++ src/Vues/v_choixLeVisiteur.php
@@ -1,72 +1,59 @@
+<div class="row">
+    <div class="container mx-auto">
+        <div class="col-md-4">
+            <form action="index.php?uc=validerFrais&action=selectionnerMoisVisiteur" 
+                  method="post" role="form">
+                <div class="form-group">
+                    <label for="unVisiteur" accesskey="v">Choisir le visiteur : </label>
+                    <select id="unVisiteur" name="unVisiteur" class="form-control">
+                        <option value="none">Choisir...</option>
+                        <?php
+                        foreach ($lesVisiteurs as $unVisiteur) {
+                            $id = $unVisiteur['id'];
+                            $nom = $unVisiteur['nom'];
+                            $prenom = $unVisiteur['prenom'];
+                            if ($id == $visiteursASelectionner) {
+                                ?>
+                                <option value="<?php echo $id ?>">
+                                    <?php echo $nom . ' ' . $prenom ?> </option>
+                                <?php
+                            }
+                            ?>    
+                        </select>
+                    </div>
 
-<div class="container mx-auto">
-    <div class="col-md-4">
-        <form action="index.php?uc=validerFrais&action=selectionnerMoisVisiteur" 
-              method="post" role="form">
-            <div class="form-group">
-                <label for="choixVisiteur" accesskey="v">Choisir le visiteur : </label>
-                <select id="choixVisiteur" name="choixVisiteur" class="form-control">
-                    <?php
-                    foreach ($lesVisiteurs as $unVisiteur) {
-                        $id = $unVisiteur['id'];
-                        $nom = $unVisiteur['nom'];
-                        $prenom = $unVisiteur['prenom'];
-                        if ($id == $visiteursASelectionner) {
-                            ?>
-                            <option selected value="<?php echo $id ?>">
-                                <?php echo $nom . ' ' . $prenom ?> </option>
-                            <?php
-                        } else {
-                            ?>
-                            <option value="<?php echo $id ?>">
-                                <?php echo $nom . ' ' . $prenom ?> </option>
-                            <?php
-                        }
-                    }
-                    ?>    </select>
             </div>
-        </form>
-    </div>
-    <div class="col-md-4">
-        <form action="index.php?uc=validerFrais&action=selectionnerMoisVisiteur" 
-              method="post" role="from">
+
             <div class="from-group">
-                <label for="lstMois" accesskey="n">Mois : </label>
-                <select id="lstMois" name="lstMois" class="form-control">
+                <label for="leMois" accesskey="n">Mois : </label>
+                <select id="leMois" name="leMois" class="form-control">
+                    <option value="none">Choisir...</option>
                     <?php
+                    $lesMois = $pdo->getLesMoisDisponibles($id);
                     foreach ($lesMois as $unMois) {
                         $mois = $unMois['mois'];
                         $numAnnee = $unMois['numAnnee'];
                         $numMois = $unMois['numMois'];
-                        if ($mois == $moisASelectionner) {
-                            ?>
-                            <option selected value="<?php echo $mois ?>">
-                                <?php echo $numMois . '/' . $numAnnee ?> </option>
-                            <?php
-                        } else {
-                            ?>
-                            <option value="<?php echo $mois ?>">
-                                <?php echo $numMois . '/' . $numAnnee ?> </option>
-                            <?php
-                        }
+                        ?>
+                        <option value="<?php echo $mois ?>">
+                            <?php echo $numMois . '/' . $numAnnee ?> </option>
+                        <?php
                     }
                     ?>    
 
-                </select>
+            </select>
 
+
+        </div>
+    </div>
+
+    <div class="col-md-4">
+        <br>
+        <input id="ok" type="submit" value="Valider" class="btn btn-success" 
+               role="button">
+        <input id="annuler" type="reset" value="Effacer" class="btn btn-danger" 
+               role="button">
         </form>
     </div>
 </div>
-    
-<div class="col-md-4">
-    <br>
- <input id="ok" type="submit" value="Valider" class="btn btn-success" 
-                   role="button">
-            <input id="annuler" type="reset" value="Effacer" class="btn btn-danger" 
-                   role="button">
 </div>
-
-    <br>
-    <br>
-    <br>
-    
