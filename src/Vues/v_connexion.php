f6cd897c64017e01ac763d8b75c903e43066206c
Maxime Vernoux <mxm.vernoux@gmail.com>
2024-01-04 17:56:32 +0100

    correction

--- src/Vues/v_connexion.php
+++ src/Vues/v_connexion.php
@@ -15,41 +15,39 @@
  * @link      https://getbootstrap.com/docs/3.3/ Documentation Bootstrap v3
  */
 ?>
-<div class="container">
-    <div class="row">
-        <div class="col-md-6 col-md-offset-3">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Identification utilisateur</h3>
-                </div>
-                <div class="panel-body">
-                    <form role="form" method="post" 
-                          action="index.php?uc=connexion&action=valideConnexion">
-                        <fieldset>
-                            <div class="form-group">
-                                <div class="input-group">
-                                    <span class="input-group-addon">
-                                        <i class="glyphicon glyphicon-user"></i>
-                                    </span>
-                                    <input class="form-control" placeholder="Login"
-                                           name="login" type="text" maxlength="45">
-                                </div>
+<div class="row">
+    <div class="col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">Identification utilisateur</h3>
+            </div>
+            <div class="panel-body">
+                <form role="form" method="post" 
+                      action="index.php?uc=connexion&action=valideConnexion">
+                    <fieldset>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="glyphicon glyphicon-user"></i>
+                                </span>
+                                <input class="form-control" placeholder="Login"
+                                       name="login" type="text" maxlength="45">
                             </div>
-                            <div class="form-group">
-                                <div class="input-group">
-                                    <span class="input-group-addon">
-                                        <i class="glyphicon glyphicon-lock"></i>
-                                    </span>
-                                    <input class="form-control"
-                                           placeholder="Mot de passe" name="mdp"
-                                           type="password" maxlength="45">
-                                </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="glyphicon glyphicon-lock"></i>
+                                </span>
+                                <input class="form-control"
+                                       placeholder="Mot de passe" name="mdp"
+                                       type="password" maxlength="45">
                             </div>
-                            <input class="btn btn-lg btn-success btn-block"
-                                   type="submit" value="Se connecter">
-                        </fieldset>
-                    </form>
-                </div>
+                        </div>
+                        <input class="btn btn-lg btn-success btn-block"
+                               type="submit" value="Se connecter">
+                    </fieldset>
+                </form>
             </div>
         </div>
     </div>
--- src/Vues/v_suiviFrais.php
+++ src/Vues/v_suiviFrais.php
@@ -1,66 +1,63 @@
 <?php ?>
 <script type="text/javascript" src="js/suiviFrais/tabSuivi.js"></script>
 <script type="text/javascript" src="js/creationXHR.js"></script>
-<div class="container">
-    <div class="panel panel-warning">
-        <div class="panel-heading">
-            <span class="glyphicon glyphicon-bookmark"></span>  Liste des fiches de frais validées
-        </div>
-        <table class="table table-striped table-hover">
-            <thead>
-                <tr>
-                    <th>Visiteurs <span class="glyphicon glyphicon-user"></span></th>
-                    <th>Mois</th>
-                    <th>Total forfait <span class="glyphicon glyphicon-euro"></span></th>
-                    <th>Total hors forfait<span class="glyphicon glyphicon-euro"></span></th>
-                    <th>Montant validé<span class="glyphicon glyphicon-euro"></span></th>
-                    <th><center>Mise en paiement</center></th>
-            </tr>
-            </thead>
-            <tbody>
-                <?php
-                foreach ($ficheFraisValid as $ficheFV) {
-                    ?>
-                    <tr idLigneVisiteur="<?php echo $ficheFV['idvisiteur'] ?>" idLigneMois="<?php echo $ficheFV['mois'] ?>">
-                        <td><?php
-                            echo $ficheFV['nom'] . ' ';
-                            echo $ficheFV['prenom'];
-                            ?></td>
-                        <td><?php
-                            echo $ficheFV['mois'];
-                            ?></td>
-                        <td><?php
-                            if (isset($ficheFV['montantFrais'])) {
-                                echo $ficheFV['montantFrais'];
-                            };
-                            ?></td><td>
-                            <?php
-                            if (isset($ficheFV['montantHorsFrais'])) {
-                                echo $ficheFV['montantHorsFrais'];
-                            };
-                            ?></td>
-                        <td montant="<?php
-                        if (isset($ficheFV['montantvalide'])) {
-                            echo $ficheFV['montantvalide'];
-                        };
-                        ?>"><?php
-                                if (isset($ficheFV['montantvalide'])) {
-                                    echo $ficheFV['montantvalide'] . '€';
-                                };
-                                ?></td>
-
-
-                        <td><center><input type="checkbox" name="checkbox" class="Checkbox" idCheckBox="<?php echo $ficheFV['idvisiteur'] . '-' . $ficheFV['mois'] ?>"></center></td>
-
-                </tr>
-            <?php };
-            ?>
-            </tbody>
-        </table>
+<div class="panel panel-warning">
+    <div class="panel-heading">
+        <span class="glyphicon glyphicon-bookmark"></span>  Liste des fiches de frais validées
     </div>
+    <table class="table table-striped table-hover">
+        <thead>
+            <tr>
+                <th>Visiteurs <span class="glyphicon glyphicon-user"></span></th>
+                <th>Mois</th>
+                <th>Total forfait <span class="glyphicon glyphicon-euro"></span></th>
+                <th>Total hors forfait<span class="glyphicon glyphicon-euro"></span></th>
+                <th>Montant validé<span class="glyphicon glyphicon-euro"></span></th>
+                <th><center>Mise en paiement</center></th>
+        </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($ficheFraisValid as $ficheFV) {
+                ?>
+                <tr idLigneVisiteur="<?php echo $ficheFV['idvisiteur'] ?>" idLigneMois="<?php echo $ficheFV['mois'] ?>">
+                    <td><?php
+                        echo $ficheFV['nom'] . ' ';
+                        echo $ficheFV['prenom'];
+                        ?></td>
+                    <td><?php
+                        echo $ficheFV['mois'];
+                        ?></td>
+                    <td><?php
+                        if (isset($ficheFV['montantFrais'])) {
+                            echo $ficheFV['montantFrais'];
+                        };
+                        ?></td><td>
+                        <?php
+                        if (isset($ficheFV['montantHorsFrais'])) {
+                            echo $ficheFV['montantHorsFrais'];
+                        };
+                        ?></td>
+                    <td montant="<?php
+                    if (isset($ficheFV['montantvalide'])) {
+                        echo $ficheFV['montantvalide'];
+                    };
+                    ?>"><?php
+                            if (isset($ficheFV['montantvalide'])) {
+                                echo $ficheFV['montantvalide'] . '€';
+                            };
+                            ?></td>
+
+
+                    <td><center><input type="checkbox" name="checkbox" class="Checkbox" idCheckBox="<?php echo $ficheFV['idvisiteur'] . '-' . $ficheFV['mois'] ?>"></center></td>
+
+            </tr>
+        <?php };
+        ?>
+        </tbody>
+    </table>
 </div>
-
-
+</div>
 <div class="card panel panel-warning">
     <div class="card-btn">
         <button id="buttonSelectionAll" type="button" class="btn-MP btn btn-info btn-sm btn-nav">Tout sélectionner</button>
