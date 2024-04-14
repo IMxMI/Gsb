fbdb61b6bf3a5e8bc9f97db3293cf3392bf45aa4
Maxime Vernoux <119664725+IMxMI@users.noreply.github.com>
2024-04-14 23:11:06 +0200

    bdd

--- resources/bdd/exportBdd.ps1
+++ resources/bdd/exportBdd.ps1
@@ -1,3 +1,3 @@
 $date = Get-Date
-$fileName = "C:\Users\maxim\source\repos\GSB\resources\bdd\gsb_frais_" + $date.Year + $date.Month +$date.Day + $date.Hour +$date.Minute + ".sql"
-C:\wamp64\bin\mariadb\mariadb10.10.2\bin\mysqldump.exe -u root gsb_frais --result-file=$fileName
+$fileName = "C:\Users\maxim\OneDrive\Documents\GitHub\Gsb\resources\bdd\gsb_frais_" + $date.Year + $date.Month +$date.Day + $date.Hour +$date.Minute + ".sql"
+C:\wamp64\bin\mariadb\mariadb11.2.2\bin\mysqldump.exe -u root gsb_frais --result-file=$fileName
